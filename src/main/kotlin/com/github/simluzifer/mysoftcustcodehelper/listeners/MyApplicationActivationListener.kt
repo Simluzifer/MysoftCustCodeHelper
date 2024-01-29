29284f69ffed8f8fbaf71b5372f097a6e8bb0b96
GitHub Action <action@github.com>
2024-01-29 06:41:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/simluzifer/mysoftcustcodehelper/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/simluzifer/mysoftcustcodehelper/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.simluzifer.mysoftcustcodehelper.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
